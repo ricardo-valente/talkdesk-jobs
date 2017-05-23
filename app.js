1f48f92f946f7643ddb8c80c6709ec744b6119ab
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-05-23 22:27:26 +0100

    Optimising environment

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-// var hbs = require('express-handlebars');
-// var app = express();
-// var index = require('./routes/index');
-//
-// // set the port of our application
-// // process.env.PORT lets the port be set by Heroku
-// var port = process.env.PORT || 8080;
-//
-// // set the view engine to ejs
-// app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/' }));
-// app.set('view engine', 'hbs');
-//
-// // make express look in the public directory for assets (css/js/img)
-// app.use(express.static(__dirname + '/public'));
-//
-// // set the home page route
-// app.get('/', function(req, res) {
-//
-//     // ejs render automatically looks in the views folder
-//     res.render('index');
-// });
-//
-// app.listen(port, function() {
-//     console.log('Our app is running on http://localhost:' + port);
-// });
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -44,10 +16,9 @@ app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __d
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// var port = process.env.PORT || 8080;
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -75,9 +46,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-// app.listen(port, function() {
-//   console.log('Our app is running on http://localhost:' + port);
-// });
 
 module.exports = app;
